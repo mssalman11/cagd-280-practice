69fd3e709aab8cabf9667a5f75b1abd9c4a4ffe8
mssalman11 <149534832+mssalman11@users.noreply.github.com>
2023-10-31 13:25:56 -0700

    added more stuff to movement

---
+++ GithubPractice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Vector3 movee;
+
+    private void MovePlayer()
+    {
+        //move
+    }
+
+    private void Jump()
+    {
+        //jump
+    }
+}
